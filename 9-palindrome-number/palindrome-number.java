5b08ff8012d6a4a3aa950e7544bea26abda5c2c2
Srijon <132880799+Srijon-paul@users.noreply.github.com>
2025-10-29 18:41:07 +0530

    Time: 4 ms (100.00%) | Memory: 44.2 MB (38.20%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -5,11 +5,9 @@ class Solution {
         }
         int num = x;
         int sum = 0;
-        int len = (int)Math.log10(num) + 1;
         while(x > 0){
             int rem = x % 10;
-            sum = sum + rem * (int)Math.pow(10, len - 1);
-            len--;
+            sum = (sum * 10) + rem;
             x /= 10;
         }
         if(sum == num){
