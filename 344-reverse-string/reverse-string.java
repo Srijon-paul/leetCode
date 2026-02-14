141290adc0c211bb0a24feb665bacce5c9eb01d1
Srijon <srijonpaul003@gmail.com>
2026-02-14 17:17:13 +0530

    Time: 5 ms (3.22%) | Memory: 50.8 MB (5.32%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length; i++){
+            st.push(s[i]);
+        }
+        int j = 0;
+        while(!st.empty()){
+            s[j] = st.pop();
+            j++;
+        }
+    }
+}
