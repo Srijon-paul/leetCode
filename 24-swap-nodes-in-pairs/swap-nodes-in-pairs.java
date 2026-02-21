f131ee39b150523a1d1290b7a3ffb8333cfb9fca
Srijon <srijonpaul003@gmail.com>
2026-02-21 18:43:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (66.55%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -10,23 +10,54 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode temp = head;
-        ListNode prev = null;
-        head = head.next;
-        while (temp != null && temp.next != null) {
-            ListNode first = temp;
-            ListNode second = temp.next;
-            first.next = second.next;
-            second.next = first;
-            if (prev != null) {
-                prev.next = second;
+        if (head == null)
+            return null;
+        int size = 2;
+        ListNode res = null;
+        ListNode prevLeft = null;
+        ListNode left = head;
+        ListNode right = null;
+        while (true) {
+            right = left;
+            for (int i = 0; i < size - 1; i++) {
+                if (right == null) {
+                    break;
+                }
+                right = right.next;
             }
-            prev = first;
-            temp = first.next;
+            if (right != null) {
+                ListNode nextLeft = right.next;
+                reverse(left, size);
+                if (prevLeft != null) {
+                    prevLeft.next = right;
+                }
+                prevLeft = left;
+                if (res == null) {
+                    res = right;
+                }
+                left = nextLeft;
+            } else {
+                if (prevLeft != null) {
+                    prevLeft.next = left;
+                }
+                if (res == null) {
+                    res = left;
+                }
+                break;
+            }
         }
-        return head;
+        return res;
+    }
+
+    public void reverse(ListNode left, int size) {
+        ListNode prev = null;
+        ListNode curr = left;
+        while (size > 0) {
+            ListNode nex = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nex;
+            size--;
+        }
     }
 }
