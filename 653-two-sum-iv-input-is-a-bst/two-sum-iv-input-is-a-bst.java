76f3fd474548b1642ef63cc02d45ae413b50bd27
Srijon <srijonpaul003@gmail.com>
2026-03-25 23:07:13 +0530

    Time: 3 ms (73.33%) | Memory: 47.4 MB (29.71%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Stack<TreeNode> asc = new Stack<>();
+    Stack<TreeNode> desc = new Stack<>();
+    public boolean findTarget(TreeNode root, int k) {
+        if(root == null){
+            return false;
+        }
+        if(root.left == null && root.right == null){
+            return false;
+        }
+        TreeNode t = root;
+        while(t != null){
+            asc.push(t);
+            t = t.left;
+        }
+        t = root;
+        while(t != null){
+            desc.push(t);
+            t = t.right;
+        }
+        TreeNode i = getSmall();
+        TreeNode j = getBig();
+        while(i != null && j != null && i.val < j.val){
+            int sum = i.val + j.val;
+            if(sum == k) return true;
+            if(sum > k){
+                j = getBig();
+            }else{
+                i = getSmall();
+            }
+        }
+        return false;
+    }
+
+    TreeNode getSmall(){
+        if(asc.isEmpty()){
+            return null;
+        }
+        TreeNode small = asc.pop();
+        TreeNode rightChild = small.right;
+        while(rightChild != null){
+            asc.push(rightChild);
+            rightChild = rightChild.left;
+        }
+        return small;
+    }
+    TreeNode getBig(){
+        if(desc.isEmpty()){
+            return null;
+        }
+        TreeNode big = desc.pop();
+        TreeNode leftChild = big.left;
+        while(leftChild != null){
+            desc.push(leftChild);
+            leftChild = leftChild.right;
+        }
+        return big;
+    }
+}
